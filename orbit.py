5b290747fb9b734616e33795a172bcc9cc04ef6e
probably-not-porter <pelibby16@earlham.edu>
2019-10-30 11:09:18 -0400

    update rendering with midpoint and target lines

--- orbit.py
+++ orbit.py
@@ -8,6 +8,7 @@ def main():
     running = True
     screen_width = 800
     screen_height = 600
+    sun_radius = 20
     draw_line = False
     line_start = None
 
@@ -28,13 +29,19 @@ def main():
             elif event.type == pygame.MOUSEBUTTONUP:
                 draw_line = False
                 print("release obj")
-
+        
+        # DRAWING
         # draw background
         screen.fill((0,0,0))
-
         # draw mouse indicator line
         if draw_line == True:
             pygame.draw.line(screen, (255,0,0), line_start, pygame.mouse.get_pos(), 2)
+        # draw sun
+        pygame.draw.circle(screen, (50,50,50), (screen_width // 2, screen_height // 2), sun_radius+10)
+        pygame.draw.circle(screen, (150,150,150), (screen_width // 2, screen_height // 2), sun_radius+5)
+        pygame.draw.circle(screen, (200,200,200), (screen_width // 2, screen_height // 2), sun_radius+2)
+        pygame.draw.circle(screen, (255,255,255), (screen_width // 2, screen_height // 2), sun_radius)
+
 
 
         # Flip the display each frame
