0b2622bca6188e433e5b3b6a7d47246fe363bc3a
probably-not-porter <pelibby16@earlham.edu>
2019-10-31 22:32:36 -0400

    allow resize

--- orbit.py
+++ orbit.py
@@ -1,80 +1,82 @@
 import pygame
+from pygame.locals import *
 import random
 import math
 import os
 
-pygame.init()
-pygame.display.set_caption('Orbit')
-gameIcon = pygame.image.load('orbitIcon.png')
-pygame.display.set_icon(gameIcon)
-
-screen_width = 800
-screen_height = 600
-G = 6.67428e-11
-sun_mass = 10e7
-
-# list for objects
-sat_ls = []
-stars_ls = []
-
-for x in range(random.randint(100,200)):
-    x_pos = random.randint(0,screen_width)
-    y_pos = random.randint(0,screen_height)
-    radius = random.randint(1,3)
-    stars_ls.append([x_pos,y_pos,radius])
-
-
-# data structure for objects
-class Satellite:
-    def __init__(self, x, y, v, r):
-        self.x_pos = x
-        self.y_pos = y
-        self.velocity = v
-        self.radius = r
-        self.point_ls = []
-        self.mass = 0.05e7
-        self.released = False
-        self.dots = []
-        self.dot_counter = 0
-        self.color = random_color()
-
-    def updatePosition(self):
-        new_x = self.x_pos + self.velocity[0] / 2
-        new_y = self.y_pos + self.velocity[1] / 2
-        self.x_pos = new_x
-        self.y_pos = new_y
-
-    def updateVelocity(self):
-        # Compute the distance of the other body.
-        sx, sy = self.x_pos, self.y_pos
-        ox, oy = screen_width / 2, screen_height / 2
-        dx = (ox-sx)
-        dy = (oy-sy)
-        d = math.sqrt(dx**2 + dy**2)
-        if d == 0:
-            raise ValueError("Collision")
-        # Compute the force of attraction
-        f = G * self.mass * sun_mass / (d**2)
-
-        # Compute the direction of the force.
-        theta = math.atan2(dy, dx)
-        fx = math.cos(theta) * f
-        fy = math.sin(theta) * f
-        velocity = ((self.velocity[0] + fx, self.velocity[1] + fy))
-        self.velocity = velocity
-        return velocity
-
-    def setVelocity(self, x_influence, y_influence):
-        self.velocity = (x_influence/10, y_influence/10)
-    
-    def createDot(self):
-        newdot = [int(self.x_pos), int(self.y_pos)]
-        self.dots.append(newdot)
-        if len(self.dots) > 100:
-            self.dots.pop(0)
-
-
 def main():
+    pygame.init()
+    pygame.display.set_caption('Orbit')
+    gameIcon = pygame.image.load('orbitIcon.png')
+    pygame.display.set_icon(gameIcon)
+
+    G = 6.67428e-11
+    sun_mass = 10e7
+    screen_width = 1920
+    screen_height = 1080
+
+    # list for objects
+    sat_ls = []
+    stars_ls = []
+
+    for x in range(random.randint(100,200)):
+        x_pos = random.randint(0,screen_width)
+        y_pos = random.randint(0,screen_height)
+        radius = random.randint(1,3)
+        stars_ls.append([x_pos,y_pos,radius])
+
+
+    # data structure for objects
+    class Satellite:
+        def __init__(self, x, y, v, r):
+            self.x_pos = x
+            self.y_pos = y
+            self.velocity = v
+            self.radius = r
+            self.point_ls = []
+            self.mass = 0.05e7
+            self.released = False
+            self.dots = []
+            self.dot_counter = 0
+            self.color = random_color()
+
+        def updatePosition(self):
+            new_x = self.x_pos + self.velocity[0] / 2
+            new_y = self.y_pos + self.velocity[1] / 2
+            self.x_pos = new_x
+            self.y_pos = new_y
+
+        def updateVelocity(self):
+            # Compute the distance of the other body.
+            sx, sy = self.x_pos, self.y_pos
+            ox, oy = screen_width / 2, screen_height / 2
+            dx = (ox-sx)
+            dy = (oy-sy)
+            d = math.sqrt(dx**2 + dy**2)
+            if d == 0:
+                raise ValueError("Collision")
+            # Compute the force of attraction
+            f = G * self.mass * sun_mass / (d**2)
+
+            # Compute the direction of the force.
+            theta = math.atan2(dy, dx)
+            fx = math.cos(theta) * f
+            fy = math.sin(theta) * f
+            velocity = ((self.velocity[0] + fx, self.velocity[1] + fy))
+            self.velocity = velocity
+            return velocity
+
+        def setVelocity(self, x_influence, y_influence):
+            self.velocity = (x_influence/10, y_influence/10)
+        
+        def createDot(self):
+            newdot = [int(self.x_pos), int(self.y_pos)]
+            self.dots.append(newdot)
+            if len(self.dots) > 100:
+                self.dots.pop(0)
+
+
+
     # VARS
     running = True
     
@@ -85,7 +87,7 @@ def main():
     line_start = None
 
     # create game screen
-    screen = pygame.display.set_mode([screen_width, screen_height])
+    screen = pygame.display.set_mode((0,0), pygame.RESIZABLE)
 
     # main logic loop
     while running:
@@ -93,6 +95,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == VIDEORESIZE:
+                x,y = event.size
+                if x>0 and y> 0:
+                    screen_width, screen_height = x,y
+                print(event.size)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 x,y = event.pos
                 line_start = (x,y)
